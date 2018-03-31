9abffac0544710b48ca7f3b1901aed853ac20647
MohanSonia <31935268+MohanSonia@users.noreply.github.com>
2018-03-31 13:00:17 +0530

    Send SMS

---
+++ SMSApp/SMSApp/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SMSApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name";
+            // 
+            // textUserName
+            // 
+            this.textUserName.Location = new System.Drawing.Point(105, 24);
+            this.textUserName.Name = "textUserName";
+            this.textUserName.Size = new System.Drawing.Size(275, 20);
+            this.textUserName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(105, 58);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.Size = new System.Drawing.Size(275, 20);
+            this.textPassword.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone Number";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(105, 96);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(275, 20);
+            this.txtPhoneNumber.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Message";
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(15, 164);
+            this.textMessage.Multiline = true;
+            this.textMessage.Name = "textMessage";
+            this.textMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textMessage.Size = new System.Drawing.Size(480, 173);
+            this.textMessage.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 397);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SMSApp/SMSApp/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMSApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                try
+                {
+                    if (textUserName.Text != string.Empty && textPassword.Text != string.Empty)
+                    {
+                        if (txtPhoneNumber.Text != string.Empty && txtPhoneNumber.TextLength != 10)
+                        {
+                            string url = "http://smsc.vianett.no/v3/send.ashx?" + "src=" + txtPhoneNumber.Text + "&" + "dst=" + txtPhoneNumber.Text + "&" +
+                            "msg=" + System.Web.HttpUtility.UrlEncode(textMessage.Text, System.Text.Encoding.GetEncoding("ISO-8859-1")) + "&" +
+                             "username=" + System.Web.HttpUtility.UrlEncode(textUserName.Text) + "&" +
+                             "password=" + System.Web.HttpUtility.UrlEncode(textPassword.Text);
+
+                            string result = client.DownloadString(url);
+                            if (result.Contains("OK"))
+                            {
+                                MessageBox.Show("Your Message is Sent Successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Sending Failes", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (txtPhoneNumber.Text != string.Empty && txtPhoneNumber.TextLength != 10)
+                        {
+                            string url = "http://smsc.vianett.no/v3/send.ashx?" + "src=" + txtPhoneNumber.Text + "&" + "dst=" + txtPhoneNumber.Text + "&" +
+                           "msg=" + System.Web.HttpUtility.UrlEncode(textMessage.Text, System.Text.Encoding.GetEncoding("ISO-8859-1")) + "&" +
+                            "username=" + System.Web.HttpUtility.UrlEncode("chandsmohan14@gmail.com") + "&" +
+                            "password=" + System.Web.HttpUtility.UrlEncode("SoniaMyJaan");
+
+                            string result = client.DownloadString(url);
+                            if (result.Contains("OK"))
+                            {
+                                MessageBox.Show("Your Message is Sent Successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Sending Failes", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
